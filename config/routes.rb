214fc35ce8b7820dac1f49a0b0ea05805b8618df
abbas <khajah@goestore.com>
2015-09-23 00:06:22 +0300

    custmoize bootstrap var

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root 'pages#home'
 
 get "about" => "pages#about" # creates about_path
+get "service" => "pages#service" 
 
 
   # The priority is based upon order of creation: first created -> highest priority.
